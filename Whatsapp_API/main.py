2492f4193dc3c754b201992c9b325532f0069717
Qwagperson <gsantelicesn2@gmail.com>
2023-05-31 20:22:43 -0400

    [Feat] Added Meta endpoint verification. Relates to #13.

--- Whatsapp_API/main.py
+++ Whatsapp_API/main.py
@@ -1,5 +1,11 @@
-from typing import Union
-from fastapi import FastAPI
+from typing import Union, Annotated
+from fastapi import FastAPI, Query, Request
+from fastapi.responses import Response
+import dotenv
+import os
+
+# Loading the secret variables
+dotenv.load_dotenv(dotenv_path="config_files/.secrets.env")
 
 app = FastAPI()
 
@@ -12,3 +18,27 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: Union[int, str]):
     return {"item_id": item_id}
+
+
+@app.get("/webhooks")
+def read_webhooks(
+        hub_mode: Annotated[str | None, Query(alias="hub.mode")] = None,
+        hub_challenge: Annotated[str | None, Query(alias="hub.challenge")] = None,
+        hub_verify_token: Annotated[str | None, Query(alias="hub.verify_token")] = None
+):
+    # Checking the 'verify token'
+    if hub_verify_token != os.environ.get("VERIFY_TOKEN"):
+        return {"error": "Invalid verify token"}
+
+    # Responding to the challenge
+    if hub_mode == "subscribe" and hub_challenge:
+        return {"hub.challenge": hub_challenge}
+
+    return {"Status": "Failure"}
+
+
+@app.post("/webhooks")
+def notice_change(request: Request):
+    # Print the request body
+    print(request.body)
+    return Response(status_code=204)
