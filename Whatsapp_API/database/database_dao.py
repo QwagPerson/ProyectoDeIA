2e2bf65f11b95b96719a8633bcca384f8c57f08c
Martinix901 <132959024+MartinGGS@users.noreply.github.com>
2023-07-16 10:26:26 -0400

    [feature] database management

---
+++ Whatsapp_API/database/database_dao.py
@@ -0,0 +1,57 @@
+from conexion import ConexionDB
+
+def crear_tabla_usuarios():
+    conexion = ConexionDB()
+    sql = '''
+    CREATE TABLE USUARIOS(
+        ID_USUARIO INTEGER NOT NULL,
+        ID_HORA DATETIME,
+        PRIMARY KEY(ID_USUARIO AUTOINCREMENT)
+    )
+    '''
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+        print('Se ha creado exitosamente la tabla')
+    except Exception as error:
+        print(error)
+
+
+def crear_tabla_horas():
+    conexion = ConexionDB()
+    sql = '''
+    CREATE TABLE HORAS(
+        ID_HORA INTEGER NOT NULL,
+        HORA DATETIME,
+        DIRECCION VARCHAR(255),
+        PRIMARY KEY(ID_USUARIO AUTOINCREMENT)
+    )
+    '''
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+        print('Se ha creado exitosamente la tabla')
+    except Exception as error:
+        print(error)
+
+def borrar_tabla():
+    conexion = ConexionDB()
+    sql = 'DROP TABLE HORAS'
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+        print('Se ha borrado la tabla con exito')
+    except Exception as error:
+        print(error)
+
+def guardar_usuario(id_user,id_hour):
+    conexion = ConexionDB()
+    sql = '''
+    INSERT INTO USUARIOS VALUES ()
+    '''
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+        print('Se ha creado exitosamente la tabla')
+    except Exception as error:
+        print(error)
